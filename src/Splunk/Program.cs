1dfeae46631727721ddd993fff9eb71d4ea51f4f
Kyle Spearrin <kyle.spearrin@gmail.com>
2022-07-07 17:27:38 -0400

    stub out splunk script app project

---
+++ src/Splunk/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bit.Splunk
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            Console.WriteLine(DateTime.UtcNow.ToString("o", CultureInfo.InvariantCulture) + 
+                " Hello World! " + rnd.Next());
+        }
+    }
+}
