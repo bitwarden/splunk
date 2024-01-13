f553a29258129124b42b96244670cbe1371a5cc7
Maciej Zieniuk <zieniuk.maciej@gmail.com>
2024-01-13 01:16:05 +0100

    config cleanup

--- src/bitwarden_event_logs.py
+++ src/bitwarden_event_logs.py
@@ -1,20 +1,14 @@
 import os
 import sys
-from typing import Optional
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
 
 import logging
 
-from config import (
-    parse_settings_config,
-    parse_bitwarden_api_key,
-    parse_checkpoint
-)
+from config import Config
 from bitwarden_api import BitwardenApi
-from models import BitwardenEventsRequest, EventLogsCheckpoint
 from mappers import get_bitwarden_api_config
-from utils import read_session_token, get_logger, set_logging_level, obj_to_json
+from utils import read_session_token, get_logger
 from splunk_api import SplunkApi
 from event_logs import EventLogsWriter
 
@@ -23,9 +17,13 @@ class App:
     def __init__(self):
         self.logger = get_logger()
         self.splunk_api = self.create_splunk_api()
-        self.settings_config = self.get_settings_config()
+
+        self.config = Config(self.splunk_api)
+        self.settings_config = self.config.get_settings_config()
+        self.bitwarden_api_key = self.config.get_bitwarden_api_key()
+        self.checkpoint = self.config.get_checkpoint()
+
         self.bitwarden_api = self.create_bitwarden_api()
-        self.checkpoint = self.get_checkpoint()
         self.event_logs_writer = self.create_event_logs_writer()
 
     def create_splunk_api(self):
@@ -34,65 +32,24 @@ class App:
 
         return SplunkApi(session_token)
 
-    def get_settings_config(self):
-        settings_config_dict = self.splunk_api.get_configuration('script')
-        settings_config = parse_settings_config(settings_config_dict)
-
-        self.logger.debug('settings config %s', settings_config)
-
-        if settings_config.logging_level is not None:
-            set_logging_level(settings_config.logging_level)
-        return settings_config
-
     def create_bitwarden_api(self):
-        bitwarden_api_key_dict = self.splunk_api.get_storage_password("bitwarden_event_logs_realm:api_key")
-        bitwarden_api_key = parse_bitwarden_api_key(bitwarden_api_key_dict)
-
-        bitwarden_api_config = get_bitwarden_api_config(self.settings_config, bitwarden_api_key)
+        bitwarden_api_config = get_bitwarden_api_config(self.settings_config,
+                                                        self.bitwarden_api_key)
 
         self.logger.debug('bitwarden api config %s', bitwarden_api_config)
 
         return BitwardenApi(bitwarden_api_config)
 
-    def get_checkpoint(self) -> EventLogsCheckpoint:
-        events_api_config_list = self.splunk_api.get_storage_configuration('eventsapi')
-        checkpoint = parse_checkpoint(events_api_config_list)
-        self.logger.debug('checkpoint %s', checkpoint)
-
-        return checkpoint
-
     def create_event_logs_writer(self):
         return EventLogsWriter(self.bitwarden_api, self.checkpoint, self.settings_config)
-
-    def update_checkpoint(self, next_request: Optional[BitwardenEventsRequest]):
-
-        last_log_date = self.checkpoint.last_log_date
-        if next_request is None and self.checkpoint.next_request is not None:
-            last_log_date = self.checkpoint.next_request.end
-
-        checkpoint = EventLogsCheckpoint(self.checkpoint.key_id,
-                                         next_request,
-                                         last_log_date)
-
-        checkpoint_json = obj_to_json(checkpoint)
-
-        if checkpoint.key_id is None:
-            self.splunk_api.create_storage_configuration('eventsapi',
-                                                         checkpoint_json)
-            self.checkpoint = self.get_checkpoint()
-        else:
-            self.splunk_api.update_storage_configuration('eventsapi',
-                                                         checkpoint.key_id,
-                                                         checkpoint_json)
-            self.checkpoint = checkpoint
 
     def run(self):
         for next_request, events in self.event_logs_writer.read_events():
             self.event_logs_writer.write_events(events)
 
-            self.update_checkpoint(next_request)
+            self.checkpoint = self.config.update_checkpoint(self.checkpoint, next_request)
 
-        self.update_checkpoint(None)
+        self.checkpoint = self.config.update_checkpoint(self.checkpoint, None)
 
 
 def main():
--- src/config.py
+++ src/config.py
@@ -7,61 +7,119 @@ from models import (
     EventLogsCheckpoint,
     BitwardenEventsRequest
 )
+from splunk_api import SplunkApi
+from utils import get_logger, set_logging_level, obj_to_json
 
 
-def parse_settings_config(settings: Optional[Dict[str, Dict[str, Any]]]) -> SettingsConfig:
-    if settings is None or 'config' not in settings:
-        raise Exception("Invalid settings configuration")
+class Config:
+    def __init__(self, splunk_api: SplunkApi):
+        self.logger = get_logger()
+        self.splunk_api = splunk_api
 
-    settings_config = settings['config']
+    def get_settings_config(self):
+        settings_config_dict = self.splunk_api.get_configuration('script')
+        settings_config = Config.__parse_settings_config(settings_config_dict)
 
-    if (settings_config is None
-            or 'apiUrl' not in settings_config
-            or 'identityUrl' not in settings_config):
-        raise Exception("Invalid settings configuration")
+        self.logger.debug('settings config %s', settings_config)
 
-    api_url: str = settings_config['apiUrl']
-    identity_url: str = settings_config['identityUrl']
+        if settings_config.logging_level is not None:
+            set_logging_level(settings_config.logging_level)
+        return settings_config
 
-    # Backward compatibility
-    api_url = api_url.strip(" \"'")
-    identity_url = identity_url.strip(" \"")
+    def get_bitwarden_api_key(self):
+        bitwarden_api_key_dict = self.splunk_api.get_storage_password("bitwarden_event_logs_realm:api_key")
+        bitwarden_api_key = Config.__parse_bitwarden_api_key(bitwarden_api_key_dict)
 
-    start_date = datetime_from_str(settings_config.get('startDate', None))
+        self.logger.debug('bitwarden api key %s', bitwarden_api_key)
 
-    return SettingsConfig(api_url,
-                          identity_url,
-                          start_date,
-                          settings_config.get('loggingLevel', None))
+        return bitwarden_api_key
 
+    def get_checkpoint(self) -> EventLogsCheckpoint:
+        events_api_config_list = self.splunk_api.get_storage_configuration('eventsapi')
+        checkpoint = Config.__parse_checkpoint(events_api_config_list)
 
-def parse_bitwarden_api_key(bitwarden_api_key: Optional[str]) -> BitwardenApiKey:
-    if bitwarden_api_key is None or "_" not in bitwarden_api_key:
-        raise Exception("Invalid Bitwarden API key")
+        self.logger.debug('checkpoint %s', checkpoint)
 
-    client_id, client_secret = bitwarden_api_key.split("_", 1)
+        return checkpoint
 
-    return BitwardenApiKey(client_id, client_secret)
+    def update_checkpoint(self,
+                          checkpoint: EventLogsCheckpoint,
+                          next_request: Optional[BitwardenEventsRequest]):
 
+        last_log_date = checkpoint.last_log_date
+        if next_request is None and checkpoint.next_request is not None:
+            last_log_date = checkpoint.next_request.end
 
-def parse_checkpoint(events_api_config_list: Optional[List[Any]]) -> EventLogsCheckpoint:
-    if events_api_config_list is None or len(events_api_config_list) == 0:
-        return EventLogsCheckpoint()
+        new_checkpoint = EventLogsCheckpoint(checkpoint.key_id,
+                                             next_request,
+                                             last_log_date)
 
-    events_api_config = events_api_config_list[0]
+        new_checkpoint_json = obj_to_json(new_checkpoint)
 
-    if events_api_config is None or '_key' not in events_api_config:
-        raise Exception("Invalid checkpoint")
+        if new_checkpoint.key_id is None:
+            self.splunk_api.create_storage_configuration('eventsapi',
+                                                         new_checkpoint_json)
+            return self.get_checkpoint()
+        else:
+            self.splunk_api.update_storage_configuration('eventsapi',
+                                                         new_checkpoint.key_id,
+                                                         new_checkpoint_json)
+            return new_checkpoint
 
-    next_request = None
-    next_request_dict = events_api_config.get('next_request', None)
-    if next_request_dict is not None:
-        next_request = BitwardenEventsRequest(datetime_from_str(next_request_dict['start']),
-                                              datetime_from_str(next_request_dict['end']),
-                                              next_request_dict.get('continuation_token', None))
+    @classmethod
+    def __parse_settings_config(cls, settings: Optional[Dict[str, Dict[str, Any]]]) -> SettingsConfig:
+        if settings is None or 'config' not in settings:
+            raise Exception("Invalid settings configuration")
 
-    last_log_date = datetime_from_str(events_api_config.get('last_log_date', None))
+        settings_config = settings['config']
 
-    return EventLogsCheckpoint(events_api_config['_key'],
-                               next_request,
-                               last_log_date)
+        if (settings_config is None
+                or 'apiUrl' not in settings_config
+                or 'identityUrl' not in settings_config):
+            raise Exception("Invalid settings configuration")
+
+        api_url: str = settings_config['apiUrl']
+        identity_url: str = settings_config['identityUrl']
+
+        # Backward compatibility
+        api_url = api_url.strip(" \"'")
+        identity_url = identity_url.strip(" \"")
+
+        start_date = datetime_from_str(settings_config.get('startDate', None))
+
+        return SettingsConfig(api_url=api_url,
+                              identity_url=identity_url,
+                              start_date=start_date,
+                              logging_level=settings_config.get('loggingLevel', None))
+
+    @classmethod
+    def __parse_bitwarden_api_key(cls, bitwarden_api_key: Optional[str]) -> BitwardenApiKey:
+        if bitwarden_api_key is None or "_" not in bitwarden_api_key:
+            raise Exception("Invalid Bitwarden API key")
+
+        client_id, client_secret = bitwarden_api_key.split("_", 1)
+
+        return BitwardenApiKey(client_id, client_secret)
+
+    @classmethod
+    def __parse_checkpoint(cls, events_api_config_list: Optional[List[Any]]) -> EventLogsCheckpoint:
+        if events_api_config_list is None or len(events_api_config_list) == 0:
+            return EventLogsCheckpoint()
+
+        events_api_config = events_api_config_list[0]
+
+        if events_api_config is None or '_key' not in events_api_config:
+            raise Exception("Invalid checkpoint")
+
+        next_request = None
+        next_request_dict = events_api_config.get('next_request', None)
+        if next_request_dict is not None:
+            next_request = BitwardenEventsRequest(start=datetime_from_str(next_request_dict['start']),
+                                                  end=datetime_from_str(next_request_dict['end']),
+                                                  continuation_token=next_request_dict.get('continuation_token', None))
+
+        last_log_date = datetime_from_str(events_api_config.get('last_log_date', None))
+
+        return EventLogsCheckpoint(events_api_config['_key'],
+                                   next_request,
+                                   last_log_date)
